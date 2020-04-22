2302bdb42cfa667c1128b5101bec4c5fa88edf95
Ishwor Khadka <khadkaishwor44@gmail.com>
2020-04-23 06:54:52 +1000

    deploying to heroku third try

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -6,25 +6,25 @@ const StripeCheckoutButton = ({ price }) => {
    const priceForStripe = price * 100;
    const publishableKey = 'pk_test_31CFnDGhm55XgAUw6JlBLtj600BAhd95MV';
 
-  const onToken = token => {
-        axios({
-            url:  'payment',
-            method: 'post',
-            data: {
-                amount: priceForStripe,
-                token: token
-            }
-        })
-        .then(response => {
-            alert('Payment Successful');
-            })
-            .catch(error => {
-                console.log('Payment error: ', error);
-                alert (
-                'There was an issue with your payment. Please make sure to use the provided credit card. '
-                );
-            });
-        };
+   const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+    };
 
     return (
         <StripeCheckout 
